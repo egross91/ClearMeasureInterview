0223c605aa6039d0c33c9cbbfb78044f8f791196
Eric Gross <eric.bgross@gmail.com>
2017-07-09 19:52:25 -0500

    Added OS check to see console output in main program in windows.

--- ClearMeaureInterview/Program.cs
+++ ClearMeaureInterview/Program.cs
@@ -25,6 +25,13 @@ namespace ClearMeaureInterview
 						Console.WriteLine(result.Result);
 					}
 				}
+
+                // NOTE: Because this project was originally created with Mac, the console window does not remain open to view results.
+                if (Environment.OSVersion.ToString().Contains("Windows"))
+                {
+                    Console.WriteLine("Press enter to continue...");
+                    Console.ReadLine();
+                }
             }
             catch (Exception e)
             {
